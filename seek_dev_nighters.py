3a618df7533080f7c5a798d1165538af7615022b
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-23 09:10:18 +0300

    Define load_attempts()

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,25 @@
+import requests
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    pages = 10
+    for page in range(1, pages + 1):
+        url = 'https://devman.org/api/challenges/solution_attempts/?page={}'
+        attempts = requests.get(url.format(page)).json()
+        for attempt in attempts['records']:
+            yield {
+                'username': attempt['username'],
+                'timestamp': attempt['timestamp'],
+                'timezone': attempt['timezone'],
+            }
 
 def get_midnighters():
+    # pytz - venv, install
+    # для него после 12 - !timezone use
+    # 1 - temiestamp 2 timezone 3 posle 24 return username
     pass
 
 if __name__ == '__main__':
-  pass
+    for i in load_attempts():
+        print(i)
+        # break
