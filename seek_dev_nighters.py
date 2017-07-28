d728a117c189b446ccaad6d055ec4aa2d38bf269
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-28 21:41:21 +0300

    Get midnighters

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -18,20 +18,22 @@ def load_attempts():
         page_content = requests.get(host, params={'page': pages})
 
 
-def get_midnighters(attempt):
-    if attempt['timestamp'] is not None:
-        local_tz = timezone(attempt['timezone'])
-        local_dt = datetime.fromtimestamp(attempt['timestamp'], tz=local_tz)
-        midnight_end_hour = 6
-        if local_dt.hour < midnight_end_hour:
-            return attempt['username']
+def get_midnighters(attempts):
+    midnighters = set()
+    for attempt in attempts:
+        if attempt['timestamp'] is not None:
+            local_tz = timezone(attempt['timezone'])
+            local_dt = datetime.fromtimestamp(attempt['timestamp'], tz=local_tz)
+            midnight_end_hour = 6
+            if local_dt.hour < midnight_end_hour:
+                midnighters.add(attempt['username'])
+    return midnighters
 
 
 if __name__ == '__main__':
-    midnighters = []
-    for attempt in load_attempts():
-        midnighters.append(get_midnighters(attempt))
+    attempts = load_attempts()
+    midnighters = get_midnighters(attempts)
     print("Midnighters:")
-    for username in set(midnighters):
+    for username in midnighters:
         if username is not None:
             print(username)
